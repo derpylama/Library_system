c94f32cdff1a9c48f4d5d376e8154806642426ac
SimonAndersson0 <151626143+SimonAndersson0@users.noreply.github.com>
2025-11-04 14:51:56 +0100

    made fetch_assoc defualt in db calls

--- php/db.php
+++ php/db.php
@@ -3,11 +3,15 @@ $host = 'localhost';
 $dbname = 'library_system';
 $user = 'root';
 $pass = '';
+$charset = 'utf8';
 
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $user, $pass, $options);
 } catch (PDOException $e) {
     die('Database connection failed: ' . $e->getMessage());
 }
