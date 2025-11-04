c7f24b398a29836e801493374ef1850241370684
derpylama <98833155+derpylama@users.noreply.github.com>
2025-11-04 09:40:51 +0100

    new css file

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Login</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
     <div class="container">
