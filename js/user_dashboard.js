82e4b14f7532ce22740b820419b4e22e4006273b
derpylama <98833155+derpylama@users.noreply.github.com>
2025-11-05 11:43:26 +0100

    now it dosent send the user to the all media view when returning media

--- js/user_dashboard.js
+++ js/user_dashboard.js
@@ -1,5 +1,19 @@
 function toggleView(view) {
+    sessionStorage.setItem('currentView', view + '-view');
     document.getElementById('media-view').classList.add('hidden');
     document.getElementById('loans-view').classList.add('hidden');
     document.getElementById(view + '-view').classList.remove('hidden');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const savedView = sessionStorage.getItem('currentView') || 'media-view';
+    toggleView(savedView.replace('-view', ''));
+
+    document.getElementById('media-view').addEventListener('click', function() {
+        toggleView('media');
+    });
+
+    document.getElementById('loans-view').addEventListener('click', function() {
+        toggleView('loans');
+    });
+})
