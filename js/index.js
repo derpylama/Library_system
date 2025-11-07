fe0b14b01325d69ced81396c95a17f8b3b5ce5ac
Bluechamele0n <108464956+Bluechamele0n@users.noreply.github.com>
2025-11-07 10:21:57 +0100

    Rename to correctness

--- js/index.js
+++ js/index.js
@@ -1,23 +1,72 @@
-let isLogin = true;
-function toggleForm() {
-    if (document.getElementById("error-message")){
-        document.getElementById("error-message").remove();
-    }
-    const formTitle = document.getElementById('form-title');
-    const action = document.getElementById('action');
-    const submitBtn = document.getElementById('submit-btn');
-    const toggleText = document.getElementById('toggle-text');
+function toggleView(view) {
+    sessionStorage.setItem('currentView', view);
+    document.getElementById('all-media-view').classList.add('hidden');
+    document.getElementById('my-account-view').classList.add('hidden');
+    document.getElementById(view).classList.remove('hidden');
+
+    const savedView = sessionStorage.getItem('currentView') || 'all-media-view';
+
+    var allMediaButton = document.getElementById("all-media-button");
     
-    if (isLogin) {
-        formTitle.innerText = 'Register';
-        action.value = 'register';
-        submitBtn.innerText = 'Create Account';
-        toggleText.innerHTML = '<a href="#" onclick="toggleForm()">Login</a>';
-    } else {
-        formTitle.innerText = 'Login';
-        action.value = 'login';
-        submitBtn.innerText = 'Login';
-        toggleText.innerHTML = '<a href="#" onclick="toggleForm()">Create account</a>';
+    var myAccountButton = document.getElementById("my-account-button");
+    if (myAccountButton !== null) {
+        if (savedView === 'my-account-view') {
+            myAccountButton.style.display = "none";
+        }
+        else{
+            myAccountButton.style.display = "inline-block";
+        }
+    }    
+
+    if (savedView === 'all-media-view') {
+        allMediaButton.style.display = "none";
     }
-    isLogin = !isLogin;
+    else{
+        allMediaButton.style.display = "inline-block";
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const savedView = sessionStorage.getItem('currentView') || 'all-media-view';
+    toggleView(savedView);
+
+    var allMediaButton = document.getElementById("all-media-button");
+    var myAccountButton = document.getElementById("my-account-button");
+
+    if (myAccountButton !== null) {
+        if (savedView === 'my-account-view') {
+            myAccountButton.style.display = "none";
+        }
+        else{
+            myAccountButton.style.display = "inline-block";
+        }
+    }  
+
+    if (savedView === 'all-media-view') {
+        allMediaButton.style.display = "none";
+    }
+    else{
+        allMediaButton.style.display = "inline-block";
+    }
+
+    document.getElementById('all-media-view').addEventListener('click', function() {
+        toggleView('all-media-view');
+    });
+
+    document.getElementById('my-account-view').addEventListener('click', function() {
+        toggleView('my-account-view');
+    });
+
+    var mediaLoanButtons = document.querySelectorAll(".media-loan-button");
+
+    mediaLoanButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            window.scrollTo(0, 0);
+
+            var messageText = document.getElementById('top-menu-message');
+            messageText.style.display = "block";
+            messageText.innerText = "You need to be logged in to be able to loan media";
+
+        });
+    });
+})
